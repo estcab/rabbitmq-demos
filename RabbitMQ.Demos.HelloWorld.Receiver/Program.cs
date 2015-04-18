268cf8cc11c91d463b1eb79dfee4d0b57f3aa4b8
estcab <estcab@gmail.com>
2015-04-18 08:56:53 +0200

    receiver file

--- RabbitMQ.Demos.HelloWorld.Receiver/Program.cs
+++ RabbitMQ.Demos.HelloWorld.Receiver/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQ.Demos.HelloWorld.Receiver
 {
@@ -10,6 +12,36 @@ namespace RabbitMQ.Demos.HelloWorld.Receiver
     {
         static void Main(string[] args)
         {
+            // open a connection and a channel, 
+            // and declare the queue from which we're going to consume
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // same as the sender queue
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    // callback in the form of an object that will 
+                    // buffer the messages until we're ready to use them
+                    var consumer = new QueueingBasicConsumer(channel);                    
+                    
+                    channel.BasicConsume("hello", true, consumer);
+
+                    Console.WriteLine(" [*] Waiting for messages.To exit press CTRL+C");
+
+                    while (true)
+                    {
+                        //blocks until another message has been delivered from the server 
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(" [x] Received {0}", message);
+                    }
+                }
+            }
         }
     }
 }
