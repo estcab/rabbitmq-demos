74c6c981324ff32b8d4f1483719267d0840efd89
estcab <estcab@gmail.com>
2015-04-18 17:04:51 +0200

    Publish/Subscribe tutorial project

---
+++ RabbitMQ.Demos.PubSub.Emit/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Demos.PubSub.Emit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // the producer can only send messages to an exchange
+                    // receives messages from producers and pushes them to queues
+                    // types: direct, topic, headers and fanout.
+
+                    channel.ExchangeDeclare("logs", "fanout");
+                }
+            }
+        }
+    }
+}
--- RabbitMQ.Demos.WorkQueues.Worker/Program.cs
+++ RabbitMQ.Demos.WorkQueues.Worker/Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 using System.Threading;
 
 namespace RabbitMQ.Demos.WorkQueues.Worker
