35cdebcfa99c66548058bee44ab93a0eba7cd99a
estcab <estcab@gmail.com>
2015-04-18 17:08:48 +0200

    pub/sub subscrier project

---
+++ RabbitMQ.Demos.PubSub.Subscriber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Demos.PubSub.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
