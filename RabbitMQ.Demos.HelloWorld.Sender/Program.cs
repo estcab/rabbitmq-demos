79d57479808ba5cd80cec13163eb6248b732ffb6
estcab <estcab@gmail.com>
2015-04-18 08:23:20 +0200

    sender code

--- RabbitMQ.Demos.HelloWorld.Sender/Program.cs
+++ RabbitMQ.Demos.HelloWorld.Sender/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace RabbitMQ.Demos.HelloWorld.Sender
 {
@@ -10,6 +11,25 @@ namespace RabbitMQ.Demos.HelloWorld.Sender
     {
         static void Main(string[] args)
         {
+            // 1. create a connection to a broker on the local machine
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                // 2. In the channel is where the api resides
+                using (var channel = connection.CreateModel())
+                {
+                    // 3. we must declare a queue for us to send to
+                    // Declaring a queue is idempotent:
+                    // it will only be created if it doesn't exist already.
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    // the message content is a byte array,
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+                }
+            }
         }
     }
 }
