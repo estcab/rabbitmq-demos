8f3c4bb6d46b0f35daeaba147212ea13a04ad324
estcab <estcab@gmail.com>
2015-04-19 15:46:56 +0200

    receiver code

--- RabbitMQ.Demos.Routing.Reciever/Program.cs
+++ RabbitMQ.Demos.Routing.Reciever/Program.cs
@@ -12,6 +12,48 @@ namespace RabbitMQ.Demos.Routing.Reciever
     {
         static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // direct exchange
+                    channel.ExchangeDeclare("direct_logs", "direct");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    if (args.Length < 1)
+                    {
+                        Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                                Environment.GetCommandLineArgs()[0]);
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+
+                    foreach (var severity in args)
+                    {
+                        // Only a message published to the exchange with 'severity' routing key 
+                        // will be routed to this queue 
+                        channel.QueueBind(queueName, "direct_logs", severity);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(queueName, true, consumer);
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                          routingKey, message);
+                    }
+                }
+            }
         }
     }
 }
