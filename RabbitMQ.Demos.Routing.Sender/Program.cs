428f5c4ab96ce1e373d0f8a037b165e8abba9a31
estcab <estcab@gmail.com>
2015-04-19 15:38:27 +0200

    routing sender code

--- RabbitMQ.Demos.Routing.Sender/Program.cs
+++ RabbitMQ.Demos.Routing.Sender/Program.cs
@@ -11,6 +11,29 @@ namespace RabbitMQ.Demos.Routing.Sender
     {
         static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // direct exchange
+                    // a message goes to the queues whose binding key 
+                    // exactly matches the routing key of the message
+                    channel.ExchangeDeclare("direct_logs", "direct");
+
+                    // binding key
+                    var severity = (args.Length > 0) ? args[0] : "info";
+
+                    var message = (args.Length > 1)
+                                ? string.Join(" ", args.Skip(1).ToArray())
+                                : "Hello World!";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("direct_logs", severity, null, body);
+                    
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+                }
+            }
         }
     }
 }
