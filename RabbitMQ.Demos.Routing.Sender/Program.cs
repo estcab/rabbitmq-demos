3e324046eaf66f7a8c264a6db5012d43e5fb5368
estcab <estcab@gmail.com>
2015-04-19 15:30:44 +0200

    routing tutorial sender project

---
+++ RabbitMQ.Demos.Routing.Sender/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Demos.Routing.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
